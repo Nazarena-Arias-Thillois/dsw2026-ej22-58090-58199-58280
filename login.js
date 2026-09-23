da7d8c477c1f7e6ed6d1e2abb902b5f8736d369f
luulazarte <luciana.lazarte@alu.frt.utn.edu.ar>
2026-09-22 21:14:42 -0300

    Página Admin-Login

--- login.js
+++ login.js
@@ -15,4 +15,19 @@ document.addEventListener('DOMContentLoaded', function() {
             alert('Usuario o contraseña incorrectos');
         }
     });
+    const togglePassword = document.getElementById('togglePassword');
+    const passwordInput = document.getElementById('password');
+    togglePassword.addEventListener('click', function() {
+    const isPassword = passwordInput.type === 'password';
+    
+    if (isPassword) {
+        // Al mostrar el texto, pones el OJO ABIERTO
+        passwordInput.type = 'text';
+        this.className = 'fa-regular fa-eye icon-right';
+    } else {
+        // Al ocultar el texto (puntos), pones el OJO TACHADO
+        passwordInput.type = 'password';
+        this.className = 'fa-solid fa-eye-slash icon-right';
+    }
 });
+}); 
